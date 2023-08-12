84d99bbf0647a15d804b4fafe8b292c73f0debcc
karthikeyan-thiyagarajan <carromkarthi36@gmail.com>
2023-08-12 19:35:12 +0530

    Initial Commit

---
+++ src/main/java/com/karthikeyan/ZeusDbRoles.java
@@ -0,0 +1,8 @@
+package com.karthikeyan;
+
+/**
+ * @author Karthikeyan on 12-08-2023
+ */
+
+public class ZeusDbRoles {
+}
---
+++ src/main/java/com/karthikeyan/ZeusException.java
@@ -0,0 +1,10 @@
+package com.karthikeyan;
+
+/**
+ * @author Karthikeyan on 12-08-2023
+ */
+
+public class ZeusException extends RuntimeException {
+    public ZeusException(String message) {
+    }
+}
